8cf78eec582a4d7f169b99b09e8eb225c0f3fa9f
Satshabad Khalsa <satshabad.music@gmail.com>
2013-02-17 15:39:35 -0800

    start on #71

--- tests/config.py
+++ tests/config.py
@@ -6,10 +6,18 @@ Replace this with more appropriate tests for your application.
 """
 
 import unittest
+import mock
+from mock import patch, DEFAULT
+import octogit.config
 
-class SimpleTest(unittest.TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ConfigTest(unittest.TestCase):
+
+    @patch('octogit.config.config')
+    def test_get_username(self, mock_config):
+        octogit.config.get_username()
+        mock_config.get.assert_called_once_with('octogit', 'username')
+        mock_config.read.assert_called_once_with(octogit.config.CONFIG_FILE)
+
+
+if __name__ == '__main__':
+    unittest.main()
